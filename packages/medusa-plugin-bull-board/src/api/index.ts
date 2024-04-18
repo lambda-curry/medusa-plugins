0f49b3eb02cb578e9dd8ddd150a1929ca23ffea8
Derek Wene <derek@lambdacurry.dev>
2024-04-18 14:18:56 -0500

    feat(event-bus-redis): add our own implementation of event-bus-redis

---
+++ packages/medusa-plugin-bull-board/src/api/index.ts
@@ -0,0 +1,63 @@
+import { createBullBoard } from '@bull-board/api';
+import { ExpressAdapter } from '@bull-board/express';
+import bodyParser from 'body-parser';
+import { Router } from 'express';
+import { Queue } from 'bullmq';
+import Redis from 'ioredis';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { authenticate } from '@medusajs/medusa';
+
+export interface IOptions {
+  eventBusRedisOptions: {
+    redisUrl: string;
+    redisOptions?: any;
+    queueName?: string;
+    queueOptions?: {
+      prefix?: string;
+    };
+  };
+  basePath?: string;
+}
+
+export default function (rootDirectory: string, options: IOptions) {
+  const router = Router();
+
+  router.use(bodyParser.json());
+
+  const eventBusOptions = options.eventBusRedisOptions;
+
+  const basePath = options.basePath ?? '/queue-ui';
+
+  const serverAdapter = new ExpressAdapter();
+
+  serverAdapter.setBasePath(basePath);
+
+  const connection = new Redis(eventBusOptions.redisUrl, {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
+    lazyConnect: true,
+    ...(eventBusOptions?.redisOptions ?? {}),
+  });
+
+  const queueConfig = {
+    prefix: 'RedisEventBusService',
+    ...(eventBusOptions.queueOptions ?? {}),
+    connection,
+  };
+
+  const queue = new Queue(
+    eventBusOptions.queueName ?? `events-queue`,
+    queueConfig
+  );
+
+  createBullBoard({
+    queues: [new BullMQAdapter(queue)],
+    serverAdapter,
+  });
+
+  router.use(basePath, authenticate());
+
+  router.use(basePath, serverAdapter.getRouter());
+
+  return router;
+}

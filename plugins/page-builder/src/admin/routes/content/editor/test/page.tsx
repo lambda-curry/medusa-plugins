8fab899e30edf7b9e24aeae116b052fc9f2edc58
Antony Durán <antony@lambdacurry.dev>
2025-03-06 10:51:53 -0600

    temporary fix(page-builder): convert page with dynamic params to static due to issue

--- plugins/page-builder/src/admin/routes/content/editor/test/page.tsx
+++ plugins/page-builder/src/admin/routes/content/editor/test/page.tsx
@@ -20,20 +20,24 @@ export async function loader({ params }: LoaderFunctionArgs) {
 const PostDetailsPage = () => {
   const { id } = useParams()
   console.log("🚀 ~ PostDetailsPage ~ id:", id)
-  const postData = useLoaderData() as Awaited<ReturnType<typeof loader>>
-  console.log("🚀 ~ PostDetailsPage ~ post:", postData)
+  // const postData = useLoaderData() as Awaited<ReturnType<typeof loader>>
+  // console.log("🚀 ~ PostDetailsPage ~ post:", postData)
+  const pageName = "Test Page"
 
   return (
     <EditorSidebarProvider>
       <EditorModal open={true}>
         <EditorModal.Content>
           <EditorModal.Header>
+            {/* EditorModal.Title is required by EditorModal.Content */}
+            <EditorModal.Title hidden={true}>{pageName}</EditorModal.Title>
             <Topbar />
           </EditorModal.Header>
           <EditorModal.Body className="flex flex-col items-center">
             <PostLayout>
-              <h1>{postData.post?.title}</h1>
-              <p>{postData.post?.description}</p>
+              {/* <h1>{postData.post?.title}</h1>
+              <p>{postData.post?.description}</p> */}
+              <h1>{pageName}</h1>
             </PostLayout>
           </EditorModal.Body>
         </EditorModal.Content>

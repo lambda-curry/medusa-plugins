0395d14ce9b2dcfa732af3e950b4c1ca1d8aea26
Antony Durán <antony@lambdacurry.dev>
2025-03-13 06:17:55 -0600

    feat(page-builder): add pageBuilderModuleEvents for post creation events
    - Introduce pageBuilderModuleEvents with a POST_CREATED event for better event handling.
    - Ensure PageBuilderService is imported for module functionality.

--- plugins/page-builder/src/modules/page-builder/index.ts
+++ plugins/page-builder/src/modules/page-builder/index.ts
@@ -1,7 +1,12 @@
-import PageBuilderService from './service'
 import { Module } from '@medusajs/framework/utils'
 
+import PageBuilderService from './service'
+
 export const PAGE_BUILDER_MODULE = 'page-builder'
+
+export const pageBuilderModuleEvents = Object.freeze({
+  POST_CREATED: 'post.created',
+})
 
 export default Module(PAGE_BUILDER_MODULE, {
   service: PageBuilderService,

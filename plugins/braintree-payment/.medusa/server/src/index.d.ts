356582b11fa69070a17f3aba7050f2db171a0b5d
Govind Diwakar <sgf@sourcegoodfood.com>
2025-08-06 11:19:16 +0000

    fix: cached tokens

--- plugins/braintree-payment/.medusa/server/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './providers/payment-braintree/src/types';
-export * from './providers/payment-braintree/src/core/braintree-base';
-export { default as BraintreePaymentProvider } from './providers/payment-braintree/src';
--- plugins/braintree-payment/src/providers/payment-braintree/src/types/index.ts
+++ plugins/braintree-payment/src/providers/payment-braintree/src/types/index.ts
@@ -20,3 +20,68 @@ export interface CustomFields {
   cart_id?: string;
   customer_id: string;
 }
+
+
+export interface DecodedClientToken {
+  version: number
+  authorizationFingerprint: string
+  configUrl: string
+  graphQL: GraphQl
+  clientApiUrl: string
+  environment: string
+  merchantId: string
+  assetsUrl: string
+  authUrl: string
+  venmo: string
+  challenges: any[]
+  threeDSecureEnabled: boolean
+  analytics: Analytics
+  paypalEnabled: boolean
+  paypal: Paypal
+}
+
+export interface GraphQl {
+  url: string
+  date: string
+  features: string[]
+}
+
+export interface Analytics {
+  url: string
+}
+
+export interface Paypal {
+  billingAgreementsEnabled: boolean
+  environmentNoNetwork: boolean
+  unvettedMerchant: boolean
+  allowHttp: boolean
+  displayName: string
+  clientId: any
+  baseUrl: string
+  assetsUrl: string
+  directBaseUrl: any
+  environment: string
+  braintreeClientId: string
+  merchantAccountId: string
+  currencyIsoCode: string
+}
+
+
+export interface DecodedClientTokenAuthorization {
+  exp: number
+  jti: string
+  sub: string
+  iss: string
+  merchant: Merchant
+  rights: string[]
+  scope: string[]
+  options: Options
+}
+
+export interface Merchant {
+  public_id: string
+  verify_card_by_default: boolean
+  verify_wallet_by_default: boolean
+}
+
+export interface Options {}

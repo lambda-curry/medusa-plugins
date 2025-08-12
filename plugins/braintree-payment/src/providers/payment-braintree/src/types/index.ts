b6f0c67b3e92136de116f2f62479a2f4447e1d71
Mohsen <mohsen@lambdacurry.dev>
2025-08-12 11:25:10 +0300

    refactor: standardize TypeScript interface properties with semicolons and removing any types

--- plugins/braintree-payment/src/providers/payment-braintree/src/types/index.ts
+++ plugins/braintree-payment/src/providers/payment-braintree/src/types/index.ts
@@ -21,67 +21,65 @@ export interface CustomFields {
   customer_id: string;
 }
 
-
 export interface DecodedClientToken {
-  version: number
-  authorizationFingerprint: string
-  configUrl: string
-  graphQL: GraphQl
-  clientApiUrl: string
-  environment: string
-  merchantId: string
-  assetsUrl: string
-  authUrl: string
-  venmo: string
-  challenges: any[]
-  threeDSecureEnabled: boolean
-  analytics: Analytics
-  paypalEnabled: boolean
-  paypal: Paypal
+  version: number;
+  authorizationFingerprint: string;
+  configUrl: string;
+  graphQL: GraphQl;
+  clientApiUrl: string;
+  environment: string;
+  merchantId: string;
+  assetsUrl: string;
+  authUrl: string;
+  venmo: string;
+  challenges: string[];
+  threeDSecureEnabled: boolean;
+  analytics: Analytics;
+  paypalEnabled: boolean;
+  paypal: Paypal;
 }
 
 export interface GraphQl {
-  url: string
-  date: string
-  features: string[]
+  url: string;
+  date: string;
+  features: string[];
 }
 
 export interface Analytics {
-  url: string
+  url: string;
 }
 
 export interface Paypal {
-  billingAgreementsEnabled: boolean
-  environmentNoNetwork: boolean
-  unvettedMerchant: boolean
-  allowHttp: boolean
-  displayName: string
-  clientId: any
-  baseUrl: string
-  assetsUrl: string
-  directBaseUrl: any
-  environment: string
-  braintreeClientId: string
-  merchantAccountId: string
-  currencyIsoCode: string
+  billingAgreementsEnabled: boolean;
+  environmentNoNetwork: boolean;
+  unvettedMerchant: boolean;
+  allowHttp: boolean;
+  displayName: string;
+  clientId: string;
+  baseUrl: string;
+  assetsUrl: string;
+  directBaseUrl: string;
+  environment: string;
+  braintreeClientId: string;
+  merchantAccountId: string;
+  currencyIsoCode: string;
 }
 
-
 export interface DecodedClientTokenAuthorization {
-  exp: number
-  jti: string
-  sub: string
-  iss: string
-  merchant: Merchant
-  rights: string[]
-  scope: string[]
-  options: Options
+  exp: number;
+  jti: string;
+  sub: string;
+  iss: string;
+  merchant: Merchant;
+  rights: string[];
+  scope: string[];
+  options: Options;
 }
 
 export interface Merchant {
-  public_id: string
-  verify_card_by_default: boolean
-  verify_wallet_by_default: boolean
+  public_id: string;
+  verify_card_by_default: boolean;
+  verify_wallet_by_default: boolean;
 }
 
 export interface Options {}
